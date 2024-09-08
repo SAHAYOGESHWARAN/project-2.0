f699008a697be9b01e1e174e1d1f290b0cf665d0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-08 11:09:43 +0530

    2.0

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  EMAIL_HOST: process.env.EMAIL_HOST,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+};
