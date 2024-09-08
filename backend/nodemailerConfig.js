66805111012a46493009ec45a21c6cfeedf1aeb7
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-08 11:36:42 +0530

    2.0

--- backend/nodemailerConfig.js
+++ backend/nodemailerConfig.js
@@ -3,6 +3,8 @@ const { EMAIL_HOST, EMAIL_USER, EMAIL_PASS } = require('./config');
 
 const transporter = nodemailer.createTransport({
   host: EMAIL_HOST,
+  port: 587, // or 465 for SSL
+  secure: false, // true for 465, false for other ports
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
